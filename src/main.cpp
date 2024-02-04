07b0a3c9ac6486f7af8c649fbce0ce9ca8547c5f
mishpro <tabatcikov2010@gmail.com>
2024-02-05 00:52:39 +0700

    positioning

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,8 @@ class $modify(DailyLevelPage){
 		auto menu = CCMenu::create();
 		auto sprite = ButtonSprite::create("Reward");
 		auto btn = CCMenuItemSpriteExtra::create(sprite, this, nullptr);
-		auto winSize = CCDirector::get()->getWinSizeInPixels();
-		btn->setPosition(winSize.width - 125, winSize.height / 2);
+		auto winSize = CCDirector::get()->getWinSize();
+		btn->setPosition(winSize.width * 0.8, winSize.height / 2);
 		menu->addChild(btn);
 		this->addChild(menu);
 		return true;
