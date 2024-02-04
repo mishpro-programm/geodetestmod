c4b01363664c7348f4200b17ec85bfb67729ae42
mishpro <tabatcikov2010@gmail.com>
2024-02-04 20:04:00 +0700

    returned default code

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,10 @@
-/**
- * Include the Geode headers.
- */
-#include <Geode/Geode.hpp>
-/**
- * Required to modify the MenuLayer class
- */
-#include <Geode/modify/CreatorLayer.hpp>
+#include <Geode/Geode.h>
+#include <Geode/modify/MenuLayer.h>
 
-/**
- * Brings cocos2d and all Geode namespaces 
- * to the current scope.
- */
 using namespace geode::prelude;
 
-/**
- * `$modify` lets you extend and modify GD's 
- * classes; to hook a function in Geode, 
- * simply $modify the class and write a new 
- * function definition with the signature of 
- * the one you want to hook.
- */
-class $modify(CreatorLayer) {
-	/**
-	 * MenuLayer::onMoreGames is a GD function, 
-	 * so we hook it by simply writing its name 
-	 * inside a $modified MenuLayer class.
-	 * 
-	 * Note that for all hooks, your signature 
-	 * has to match exactly - `bool onMoreGames` 
-	 * would not place a hook!
-	 */
-	void onDailyLevel(CCObject*) {
-		LoadingLayer::create(true);
-	} 
-};
+class $modify(MenuLayer){
+	void onMoreGames(CCObject*){
+		FLAlertLayer::create("More Games", "Bro just type in Play Market \"Geometry Dash\"", "OK")->show();
+	}
+}
