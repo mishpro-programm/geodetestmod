d8cdec19c7a8a723e3692f859333da48efbc2c10
mishpro <tabatcikov2010@gmail.com>
2024-02-05 00:43:36 +0700

    daily

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/DailyLevelPage.hpp>
 
 using namespace geode::prelude;
 
-class $modify(MenuLayer){
-	virtual bool init(){
-		if(!MenuLayer::init()){
+class $modify(DailyLevelPage){
+	bool init(GJTimedLevelType p0){
+		if(!DailyLevelPage::init(p0)){
 			return false;
 		}
 		auto menu = CCMenu::create();
-		auto sprite = ButtonSprite::create("Test");
+		auto sprite = ButtonSprite::create("Reward");
 		auto btn = CCMenuItemSpriteExtra::create(sprite, this, nullptr);
+		auto winSize = CCDirector::get()->getWinSizeInPixels();
+		btn.setPosition(winSize.width - 125, winSize.height / 2);
 		menu->addChild(btn);
 		this->addChild(menu);
 		return true;
