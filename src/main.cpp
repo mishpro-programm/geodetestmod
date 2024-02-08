e63454812e128c2bda27429142f0c6f4be8fa982
mishpro <tabatcikov2010@gmail.com>
2024-02-09 00:29:57 +0700

    hope it will work...

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,24 @@
 using namespace geode::prelude;
 
 class $modify(MenuLayer){
-	void onMoreGames(CCObject*){
-		FLAlertLayer::create("Fact:", "Mat sdoxla ot geroina papa ushel da i huy s nim", "zhiza")->show();
+	void onRobTop(CCObject*){
+		auto scene = CCScene::create();
+		auto layer = MyLayer::create();
+		scene->addChild(layer);
+		auto transition = CCTransitionFade::create(0.5f, scene);
+		auto director = CCDirector::sharedDirector();
+		director->replaceScene(transition);
 	}
 };
+class MyLayer : CCLayer {
+	bool init(){
+		if(!CCLayer::init()) return false;
+		auto menu = CCMenu::create();
+		auto sprite = ButtonSprite::create("Button");
+		auto btn = CCMenuItemSpriteExtra::create(sprite, this, nullptr);
+		btn->setPosition(100.0f, 100.0f);
+		menu->addChild(btn);
+		this->addChild(menu);
+		return true;
+	}
+}
